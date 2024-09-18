5697e6262de5792235b7ffac6e3dd013cff83236
Tali-Scialom <tali.scialom.ext@louisvuitton.com>
2024-09-18 10:20:19 +0300

    init with kitting feature

--- force-twisticonics/main/default/lwc/icx_orderActionDeclareFundReception/icx_orderActionDeclareFundReception.js
+++ force-twisticonics/main/default/lwc/icx_orderActionDeclareFundReception/icx_orderActionDeclareFundReception.js
@@ -1,7 +1,7 @@
 import { api, track, wire } from 'lwc';
 import LightningModal from 'lightning/modal';
 
-import {getRecord } from 'lightning/uiRecordApi';
+import { getRecord } from 'lightning/uiRecordApi';
 
 import USER_ID from '@salesforce/user/Id';
 import NAME_FIELD from '@salesforce/schema/User.Name';
@@ -9,6 +9,7 @@ import RMS_ID_FIELD from '@salesforce/schema/User.RMS_ID__c';
 import WWEMPLOYEENUMBER_FIELD from '@salesforce/schema/User.WWEmployeeNumber__c';
 
 import actionFundsReception from '@salesforce/apex/Account_OrderDetailsControllerLC.sendActionFundsReception';
+import callToFinor from '@salesforce/apex/Account_OrderDetailsControllerLC.callToFinor';
 
 export default class Ics_orderActionDeclareFundReception extends LightningModal {
     @api orderdetailsapi;
@@ -16,13 +17,13 @@ export default class Ics_orderActionDeclareFundReception extends LightningModal 
     @api orderaction; // [{ label: 'Exchange', value: 'exchange' }, { label: 'Return', value: 'return' }, ...]
 
     @track isLoading = false;
-    
+
     @track userId = USER_ID;
 
     paymentMethods = [
-        {label:'Bank Transfer' ,value:'BANK_TRANSFER'},
-        {label:'Cash on Delivery' ,value:'COD'},
-        {label:'Credit Card' ,value:'CREDIT_CARD'}
+        { label: 'Bank Transfer', value: 'BANK_TRANSFER' },
+        { label: 'Cash on Delivery', value: 'COD' },
+        { label: 'Credit Card', value: 'CREDIT_CARD' }
     ];
 
     paymentMethodSelected;
@@ -35,26 +36,26 @@ export default class Ics_orderActionDeclareFundReception extends LightningModal 
     @wire(getRecord, {
         recordId: "$userId",
         fields: [NAME_FIELD, RMS_ID_FIELD, WWEMPLOYEENUMBER_FIELD]
-      }) wireuser({error, data}) {
-          console.log('JGU-icx_orderHighlightPanel-@wire (getRecord User - data) : '+JSON.stringify(data));
-          // console.log('JGU-@wire (getRecord User - error) : '+JSON.stringify(error));
-          if (error) {
-            this.errorUser = error ; 
-          } else if (data) {
-              this.wwemployeeid = data.fields.WWEmployeeNumber__c.value;
-          }
-      }
-    
+    }) wireuser({ error, data }) {
+        console.log('JGU-icx_orderHighlightPanel-@wire (getRecord User - data) : ' + JSON.stringify(data));
+        // console.log('JGU-@wire (getRecord User - error) : '+JSON.stringify(error));
+        if (error) {
+            this.errorUser = error;
+        } else if (data) {
+            this.wwemployeeid = data.fields.WWEmployeeNumber__c.value;
+        }
+    }
 
-    connectedCallback() {            
+
+    connectedCallback() {
         this.orderAmount = this.orderdetailsapi?.transactionInfo.total;
         this.currency = this.orderdetailsapi?.transactionInfo.currency_Z;
     }
 
     // When change the value in  "reason" combobox
-    handlePaymentMethodChange(event) {        
+    handlePaymentMethodChange(event) {
         setTimeout(() => (this.template.querySelector('lightning-combobox').reportValidity(), this.myValue = null))
-        this.paymentMethodSelected = event.detail.value;        
+        this.paymentMethodSelected = event.detail.value;
     }
 
     handleCancel() {
@@ -65,67 +66,44 @@ export default class Ics_orderActionDeclareFundReception extends LightningModal 
         this.isLoading = true;
         this.disableClose = true;
 
-          
-        
-        // jsonRecord = {
+        // var bodyToSend = {
         //     "order_id": this.orderdetailsapi.order_id,
-        //     "request_id":,
         //     "requesting_system": "ICONICS",
-        //     "amount": this.orderAmount,
+        //     "amount": parseFloat(+this.orderAmount),
         //     "payment_method": this.paymentMethodSelected,
         //     "transaction_type": "CAPTURE",
-        //     "transaction_time":,
-        //     "emplopyee_id": "LV123456"
+        //     "employee_id": this.wwemployeeid
         // }
 
-        var bodyToSend  = {
-            "order_id": this.orderdetailsapi.order_id,
-            "requesting_system": "ICONICS",
-            "amount": parseFloat(+this.orderAmount),
-            "payment_method": this.paymentMethodSelected,
-            "transaction_type": "CAPTURE",
-            "employee_id": this.wwemployeeid
-        }
-
-        console.log('Declare Funds Reception: '+JSON.stringify(bodyToSend ));
-
-        var response;
         var jsonResponse = [];
-
-       
-
-        //             console.log('JGU-bodyToSend:'+JSON.stringify(bodyToSend));
-        //             console.log('JGU-orderID:' + this.orderid);
-        //             console.log('JGU-orderaction:' + this.orderaction.value);
-
-
-
 
         if (this.paymentMethodSelected) {
 
-            let orderNumber = this.orderdetailsapi.order_id;
-            console.log('JGU-orderNumber2:' + orderNumber);
-
             await actionFundsReception({
-                body : bodyToSend,
-                orderNumber : this.orderdetailsapi.order_id,
-                orderAction : this.orderaction.value
+                amount: this.orderAmount,
+                paymentMethod: this.paymentMethodSelected,
+                transactionType: "CAPTURE",
+                employeeId: this.wwemployeeid,
+                orderAction: this.orderaction.value,
+                orderNumber: this.orderdetailsapi.order_id,
+                requestId: this.orderdetailsapi.order_id,
             })
-            .then(result => {           
-                console.log('JGU-result OK: '+JSON.stringify(result));
-                jsonResponse.push(result);
-            })
-            .catch(error => {
-                console.log('JGU-result Error: '+JSON.stringify(error));
-                jsonResponse.push(error);
-            });       
+                .then(result => {
+                    console.log('JGU-result OK: ' + JSON.stringify(result));
+
+                    jsonResponse.push(result);
+                })
+                .catch(error => {
+                    console.log('JGU-result Error: ' + JSON.stringify(error));
+                    jsonResponse.push(error);
+                });
 
             this.isLoading = false;
             this.disableClose = false;
 
             this.close(jsonResponse);
         }
-        else {            
+        else {
             this.isLoading = false;
             this.disableClose = false;
         }
